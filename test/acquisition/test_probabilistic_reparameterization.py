cfd43cbf84daf88f17f9042b979233f33fe9e19c
Toby Boyne <tobyboyne@gmail.com>
2025-10-20 14:44:35 +0100

    Refactor PR code to be grouped

--- test/acquisition/test_probabilistic_reparameterization.py
+++ test/acquisition/test_probabilistic_reparameterization.py
@@ -5,14 +5,17 @@ import torch
 from botorch.acquisition import LogExpectedImprovement, qLogExpectedImprovement
 from botorch.acquisition.probabilistic_reparameterization import (
     AnalyticProbabilisticReparameterization,
+    AnalyticProbabilisticReparameterizationInputTransform,
+    get_probabilistic_reparameterization_input_transform,
     MCProbabilisticReparameterization,
+    MCProbabilisticReparameterizationInputTransform,
 )
 from botorch.generation.gen import gen_candidates_scipy, gen_candidates_torch
 from botorch.models import MixedSingleTaskGP
 from botorch.models.transforms.factory import get_rounding_input_transform
 from botorch.models.transforms.input import (
-    AnalyticProbabilisticReparameterizationInputTransform,
-    MCProbabilisticReparameterizationInputTransform,
+    ChainedInputTransform,
+    Normalize,
     OneHotToNumeric,
 )
 from botorch.optim import optimize_acqf, optimize_acqf_mixed
@@ -20,6 +23,7 @@ from botorch.test_functions.synthetic import Ackley, AckleyMixed
 from botorch.utils.sampling import draw_sobol_samples
 from botorch.utils.test_helpers import get_model
 from botorch.utils.testing import BotorchTestCase
+from botorch.utils.transforms import unnormalize
 
 
 def get_categorical_features_dict(feature_to_num_categories: dict[int, int]):
@@ -178,7 +182,7 @@ class TestProbabilisticReparameterizationInputTransform(BotorchTestCase):
             **transform_params,
         )
 
-        X = torch.rand(4, 1, 1, bounds.shape[1])
+        X = torch.rand(4, 1, 1, bounds.shape[1], **self.tkwargs)
 
         with self.assertRaisesRegex(ValueError, "`n`"):
             tf.transform(X.expand(-1, -1, 2, -1))
@@ -242,6 +246,158 @@ class TestProbabilisticReparameterizationInputTransform(BotorchTestCase):
             ].all()
         )
         self.assertAllClose(X_transformed_mc[..., -1].mean().item(), 0.95, atol=0.10)
+
+
+class TestGetProbabilisticReparameterizationInputTransform(BotorchTestCase):
+    def test_get_probabilistic_reparameterization_input_transform(self):
+        for dtype in (torch.float, torch.double):
+            for use_analytic in (True, False):
+                with self.subTest(dtype=dtype, use_analytic=use_analytic):
+                    self._test_get_probabilistic_reparameterization_input_transform(
+                        dtype=dtype, use_analytic=use_analytic
+                    )
+
+    def _test_get_probabilistic_reparameterization_input_transform(
+        self, dtype: torch.dtype, use_analytic: bool
+    ):
+        expected_pr_transform_cls = (
+            AnalyticProbabilisticReparameterizationInputTransform
+            if use_analytic
+            else MCProbabilisticReparameterizationInputTransform
+        )
+
+        one_hot_bounds = torch.tensor(
+            [
+                [0, 5],
+                [0, 4],
+                [0, 1],
+                [0, 1],
+                [0, 1],
+                [0, 1],
+                [0, 1],
+            ],
+            dtype=dtype,
+            device=self.device,
+        ).t()
+
+        with self.assertRaises(ValueError):
+            # test no integer or categorical
+            get_probabilistic_reparameterization_input_transform(
+                one_hot_bounds=one_hot_bounds,
+            )
+
+        categorical_features = {2: 2, 4: 3}
+        integer_indices = [1]
+        mc_samples = 64
+        resample = False
+        tau = 0.2
+        # test both integer and categorical
+        tf = get_probabilistic_reparameterization_input_transform(
+            one_hot_bounds=one_hot_bounds,
+            integer_indices=integer_indices,
+            categorical_features=categorical_features,
+            use_analytic=use_analytic,
+            mc_samples=mc_samples,
+            resample=resample,
+            tau=tau,
+        )
+        self.assertIsInstance(tf, ChainedInputTransform)
+        tfs = list(tf.items())
+
+        self.assertEqual(len(tfs), 3)
+        tf_name_i, tf_i = tfs[0]
+        self.assertEqual(tf_name_i, "unnormalize")
+        self.assertIsInstance(tf_i, Normalize)
+        self.assertTrue(tf_i.reverse)
+        bounds = one_hot_bounds[:, integer_indices]
+        offset = bounds[:1, :]
+        coefficient = bounds[1:2, :] - offset
+        self.assertTrue(torch.equal(tf_i.coefficient, coefficient))
+        self.assertTrue(torch.equal(tf_i.offset, offset))
+        self.assertEqual(tf_i._d, one_hot_bounds.shape[1])
+        self.assertEqual(
+            tf_i.indices, torch.tensor(integer_indices, device=self.device)
+        )
+
+        tf_name_i, tf_i = tfs[1]
+        self.assertEqual(tf_name_i, "round")
+        self.assertIsInstance(tf_i, expected_pr_transform_cls)
+        self.assertEqual(tf_i.categorical_features, categorical_features)
+        self.assertEqual(tf_i.tau, tau)
+        if not use_analytic:
+            self.assertEqual(tf_i.mc_samples, mc_samples)
+            self.assertEqual(tf_i.resample, resample)
+
+        tf_name_i, tf_i = tfs[2]
+        self.assertEqual(tf_name_i, "normalize")
+        self.assertIsInstance(tf_i, Normalize)
+        self.assertFalse(tf_i.reverse)
+        self.assertTrue(torch.equal(tf_i.coefficient, coefficient))
+        self.assertTrue(torch.equal(tf_i.offset, offset))
+        self.assertEqual(tf_i._d, one_hot_bounds.shape[1])
+
+        # test forward
+        X = torch.rand(
+            4, 1, 1, one_hot_bounds.shape[1], dtype=dtype, device=self.device
+        )
+        X_tf = tf(X)
+        # assert the continuous param is unaffected
+        expected_samples = (
+            tf["round"].all_discrete_options.shape[0]
+            if use_analytic
+            else tf["round"].mc_samples
+        )
+        self.assertEqual(expected_samples, X_tf.shape[-3])
+        X_expanded = X.expand(*X.shape[:-3], expected_samples, *X.shape[-2:])
+        self.assertTrue(torch.equal(X_tf[..., 0], X_expanded[..., 0]))
+        # check that integer params are rounded
+        X_tf_int = unnormalize(X_tf, bounds=one_hot_bounds)[..., integer_indices]
+        self.assertTrue(torch.all(X_tf_int % 1 == 0))
+        # check that categoricals are discretized - check onehot constraint
+        for start, card in categorical_features.items():
+            end = start + card
+            self.assertTrue(torch.all(X_tf[..., start:end].sum(dim=-1) == 1))
+        # test transform on train/eval/fantasize
+        for tf_i in tf.values():
+            self.assertFalse(tf_i.transform_on_train)
+            self.assertTrue(tf_i.transform_on_eval)
+            # self.assertFalse(tf_i.transform_on_fantasize)
+
+        # test categoricals, no integers
+        tf = get_probabilistic_reparameterization_input_transform(
+            one_hot_bounds=one_hot_bounds,
+            integer_indices=None,
+            categorical_features=categorical_features,
+            use_analytic=use_analytic,
+            mc_samples=mc_samples,
+            resample=resample,
+            tau=tau,
+        )
+        tfs = list(tf.items())
+        self.assertEqual(len(tfs), 1)
+        tf_name_i, tf_i = tfs[0]
+        self.assertEqual(tf_name_i, "round")
+        self.assertIsInstance(tf_i, expected_pr_transform_cls)
+        self.assertIsNone(tf_i.integer_indices)
+        self.assertEqual(tf_i.categorical_features, categorical_features)
+
+        # test integers, no categoricals
+        integer_indices = [one_hot_bounds.shape[1] - 1]
+        tf = get_probabilistic_reparameterization_input_transform(
+            one_hot_bounds=one_hot_bounds,
+            integer_indices=integer_indices,
+            categorical_features=None,
+            use_analytic=use_analytic,
+            mc_samples=mc_samples,
+            resample=resample,
+            tau=tau,
+        )
+
+        tfs = list(tf.items())
+        self.assertEqual(len(tfs), 3)
+        _, tf_i = tfs[1]
+        self.assertEqual(tf_i.integer_indices.tolist(), integer_indices)
+        self.assertIsNone(tf_i.categorical_features)
 
 
 class TestProbabilisticReparameterization(BotorchTestCase):
